6bedcc18d06325e189ada7ba7ed0e93e98c3975d
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-06 10:40:36 +0000

    Finished Rails sample app: chatper 6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,20 +8,29 @@ class ListsController < ApplicationController
 
     list.save
 
-    redirect_to "/top"
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
